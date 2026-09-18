32f636a9cac07077e557d4103b84d1da290b9803
INAPP <imapp.help@gmal.com>
2026-09-17 20:48:17 -0700

    fix: run sftpgo as root after chmod 777 volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,4 @@ set -e
 # chmod 777 the volume so sftpgo can write host keys and data.
 chmod 777 /var/lib/sftpgo 2>/dev/null || true
 
-# Drop from root to uid 1000 before executing sftpgo
-exec su -s /bin/sh -c 'exec "$@"' sftpgo -- sftpgo serve
+exec sftpgo serve
